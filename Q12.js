937a0d0c5c86a25d46fa863d70ba8f5b14ca3f49
yusraimran2 <yusraimran12@hotmail.com>
2024-11-12 20:55:54 +0500

    Add files via upload

---
+++ Q12.js
@@ -0,0 +1,8 @@
+"use strict";
+// 12: Greetings:Start with the array you used in excercise 11,but instead of just printing each person's name,print a message to them.The text of each messages should be the same,but each message should be personalized with the person's name.
+let namess = ["Faryal", "Novish", "Nazia", "Umaima"];
+let msg1 = "How are you dear?";
+console.log(namess[0] + " " + msg1);
+console.log(namess[1] + " " + msg1);
+console.log(namess[2] + " " + msg1);
+console.log(namess[3] + " " + msg1);
